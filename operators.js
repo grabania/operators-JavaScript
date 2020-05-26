d4bb5e2218fd92aee4c7483552cc26f8a986bbc5
grabania <grabania.work@gmail.com>
2020-05-27 00:20:02 +0100

    Update JS

--- operators.js
+++ operators.js
@@ -1,7 +1,7 @@
 document.querySelector('button').addEventListener('click', addNumbers);
 
 function addNumbers() {
-	const number1 = Number(document.querySelector('#number1').value);
+	const number1 = Number(document.querySelector('#number1').value); //I can use + operator instead of Number to covert value into a number
 	const number2 = Number(document.querySelector('#number2').value);
 
 	const result = number1 + number2;
